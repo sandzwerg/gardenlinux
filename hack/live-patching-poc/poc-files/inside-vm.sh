8f842d21a04caeca3b9b701637162be6a28a75c1
Vincent Riesop <vincent.riesop@gmail.com>
2023-02-17 17:49:11 +0100

    hack(poc): livepatching improvements
    * fix ssh issue (use rsa instead of ecdsa)
    * update dependency list in readme
    * fix inside-vm relative path issue

--- hack/live-patching-poc/poc-files/inside-vm.sh
+++ hack/live-patching-poc/poc-files/inside-vm.sh
@@ -3,16 +3,14 @@
 set -Eeuo pipefail
 
 echo "start" 
+thisDir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 
 echo "### Install kpatch"
-./kpatch/install.sh
+$thisDir/kpatch/install.sh
 
 echo "### Do kernel Live patch"
-
 kpatch-build poc-files/hello-meminfo-5.15.76.patch
-
-
 
 echo "### Test patched kernel: Trigger patched function"
 cat /proc/meminfo
