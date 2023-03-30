e2ca8eb7315d65698589e0030e9102ccdcb0c6ad
Vincent Riesop <vincent.riesop@gmail.com>
2023-03-30 14:10:10 +0200

    fix(test): use containerd ctr instead of docker

--- features/khost/test/test_k8s_conformance.py
+++ features/khost/test/test_k8s_conformance.py
@@ -11,7 +11,8 @@ from helper.utils import execute_remote_command
 )
 
 def test_k8s_conformance(client, config_dir, non_chroot):
-    cmd = f"sudo docker run -it --rm --privileged --net=host \
-        -v /:/rootfs -v {config_dir}:{config_dir} \
+    cmd = f"sudo ctr run --rm --privileged --net-host \
+        --mount type=bind,src=$PWD,dst=/rootfs,options=rbing:rw \
+        --mount type=bind,src={config_dir},dst={config_dir},options=rbing:rw \
         registry.k8s.io/node-test:0.2"
     execute_remote_command(client, cmd)
